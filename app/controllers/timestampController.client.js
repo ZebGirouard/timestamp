7893ecacc4a32d64dcad564968976835e00270e4
Timothy Zeb Girouard <timzebgir@gmail.com>
2016-01-31 05:40:12 +0000

    Heroku modifications

--- app/controllers/timestampController.client.js
+++ app/controllers/timestampController.client.js
@@ -3,32 +3,8 @@
 (function() {
     var app = angular.module('clementineApp', ['ngResource', 'ngRoute']);
         app.controller('timestampCTRL', ['$scope', '$resource', '$route', '$routeParams', '$location', function ($scope, $resource, $route, $routeParams, $location) {
-            //var Surveys = $resource('/api/{id}/surveys');
-            //var SurveyRemove = $resource('/api/{id}/surveyRemove');
             $scope.result = {};
             $scope.choice = {};
             var allPath = $location.absUrl().split("/");
-            //$scope.surveyPath = allPath[allPath.length - 1];
-            //$scope.surveyToShow = decodeURIComponent($scope.surveyPath);
-            //$scope.userToShow = allPath[allPath.length - 3];
-            console.log(allPath);
-            //var SurveyVote = $resource('/'+$scope.userToShow+'/survey/'+$scope.surveyPath);
-            
-            
-            /*$scope.submitPoll = function () {
-                var newSurvey = $scope.survey;
-                console.log(newSurvey);
-                Surveys.save(newSurvey, function () {
-                    $scope.getSurveys();
-                });
-                $scope.newSurveyShow = false;
-                $scope.surveyNamePath = encodeURIComponent($scope.survey.name);
-                var links = document.querySelector('#surveyLink') || null;
-                if (links !== null) {
-                    links.setAttribute("href", links.href.replace('pollNameHere',$scope.surveyNamePath));
-                }
-                $scope.viewLink();
-            };*/
-            
         }]);
 })();
--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 'use strict';
 
-var express = require('express'),
-    routes = require('./app/routes/index.js'),
-    mongo = require('mongodb').MongoClient;
+var express = require('express');
+var routes = require('./app/routes/index.js');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
-  if (err) {
-    throw new Error('Database failed to connect!');
-  } else {
-    console.log('MongoDB successfully connected on port 27017.');
-  }
-  
-  app.use('/public', express.static(process.cwd() + '/public'));
-  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-  
-  routes(app, db);
-});
+require('dotenv').load();
+require('./app/config/passport')(passport);
 
-app.listen(8080, function() {
-  console.log('Listening on port 8080...');
+console.log(process.env);
+var mongoURI = process.env.MONGOLAB_URI || process.env.MONGO_URI;
+
+mongoose.connect(mongoURI);
+
+app.use(bodyParser());
+    
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
+
+app.use(session({
+	secret: 'secretClementine',
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
 });
